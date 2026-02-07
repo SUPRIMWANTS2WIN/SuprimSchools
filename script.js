d1b552ade62cb691f51bad1e56d453bfc77d65ea
SUPRIMWANTS2WIN <gautamsuprim0@gmail.com>
2026-02-07 20:57:39 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function signin(){
+window.location.href="signin.html";
+}
+function HTML(){
+	window.location.href="HTML.html";
+}
+
+function tutorials(){
+window.location.href="tutorials.html";
+}
+
+function references(){
+window.location.href="references.html";
+}
+
+function exercises(){
+window.location.href="exercises.html";
+}
